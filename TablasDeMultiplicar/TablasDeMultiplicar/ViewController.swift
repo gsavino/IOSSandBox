0051adc03f411cfcb16342cc1d562f575e56c903
Gaston Savino <gaston.savino@gmail.com>
2018-05-03 21:24:06 -0300

    botones redondeados

--- TablasDeMultiplicar/TablasDeMultiplicar/ViewController.swift
+++ TablasDeMultiplicar/TablasDeMultiplicar/ViewController.swift
@@ -11,7 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var DisplayTxtEntry: UITextField!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var buttonGo: UIButton!
     
+    @IBOutlet weak var buttonDel: UIButton!
+    @IBOutlet weak var buttonBlank: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button2: UIButton!
     var valor = 0
     
     @IBOutlet weak var testModeSwitch: UISwitch!
@@ -32,6 +45,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         DisplayTxtEntry.text = ""
+        redondeoBotones()
+    }
+    
+    func redondeoBotones(){
+        redondeoBoton(boton: button1)
+        redondeoBoton(boton: button2)
+        redondeoBoton(boton: button3)
+        redondeoBoton(boton: button4)
+        redondeoBoton(boton: button5)
+        redondeoBoton(boton: button6)
+        redondeoBoton(boton: button7)
+        redondeoBoton(boton: button8)
+        redondeoBoton(boton: button9)
+        redondeoBoton(boton: button0)
+        redondeoBoton(boton: buttonDel)
+        redondeoBoton(boton: buttonBlank)
+        redondeoBoton(boton: buttonGo)
+    }
+    
+    func redondeoBoton(boton: UIButton){
+        boton.layer.cornerRadius = 5
+        boton.layer.borderWidth = 1
+        boton.layer.borderColor = UIColor.black.cgColor
     }
 
     override func didReceiveMemoryWarning() {
